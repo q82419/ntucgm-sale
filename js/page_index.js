919653cf600f04255b5033ff53fb1d69ea9e768f
q82419 <q82419@gmail.com>
2016-03-09 00:09:56 +0800

    page index js file

---
+++ js/page_index.js
@@ -0,0 +1,13 @@
+function displayResult(type){
+    if(type == 1){
+        $('.productlist_g').toggleClass('productlist_g productlist_l');
+    }
+    else if(type == 2){
+        $('.productlist_l').toggleClass('productlist_l productlist_g');
+    }
+    return false;
+}
+
+
+$(document).ready(function(){
+});
