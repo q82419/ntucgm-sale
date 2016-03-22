f6daafc62dc46c14b09712a1e9ae269ab4df978e
q82419 <q82419@gmail.com>
2016-03-22 11:09:39 +0800

    js

---
+++ js/page_purchase.js
@@ -0,0 +1,10 @@
+$(document).keypress(function(e) {
+    if((e.keyCode || e.which) == 13) {
+        $('#searchmainsubmit a').trigger('click');
+    }
+});
+
+$(document).ready(function(){
+    initSearchBar(false);
+    submitListener(false);
+});
